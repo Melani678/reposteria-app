3f0fae2e365628c29d269a946aeaa6cb8af5b80a
Melani <mmascovargas@gmail.com>
2026-06-09 03:37:28 -0400

    frontend listo

--- reposteria-backend/src/main.ts
+++ reposteria-backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors({
-    origin: "*"
+    origin: "https://reposteria-frontend.onrender.com"
   }));
   app.use(
     '/uploads',
