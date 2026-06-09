b9174114e2963f2f6e4f7ceea633a40398820d9e
Melani <mmascovargas@gmail.com>
2026-06-09 03:31:32 -0400

    frontend listo

--- reposteria-backend/src/main.ts
+++ reposteria-backend/src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import * as express from 'express';
+import cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.use(cors({
+    origin: "*"
+  }));
   app.use(
     '/uploads',
     express.static(
